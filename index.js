2f880a0dfbb5f65f8c0e3d286a3e6bcae3fbab96
Boris Ushakov <inlvert@gmail.com>
2025-04-03 00:25:58 +0300

    add create basic server

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const { app } = require('./app');
+const {PORT} = require('./constants')
+
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`server started on ${PORT}`)
+})

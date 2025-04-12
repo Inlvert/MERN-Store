54893ee0c50a26f22b500c8d52481873ddd64053
Boris Ushakov <inlvert@gmail.com>
2025-04-13 01:05:22 +0300

    add mw in App

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = require('./routers');
+const basicErrorHendler = require("./middlewares/errors/basic.mw");
+const tokenErrorHendler = require("./middlewares/errors/tokenError.mw");
 
 
 const app = express();
 
 app.use(express.json());
-app.use(router)
+app.use(router);
+app.use(tokenErrorHendler);
+app.use(basicErrorHendler);
+
 
 module.exports.app = app;

02e6677941163a50188821fbbe739e51de02b646
Boris Ushakov <inlvert@gmail.com>
2025-04-13 11:58:07 +0300

    add cors lib + connect to app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const router = require('./routers');
+const express = require("express");
+const cors = require("cors");
+const router = require("./routers");
 const basicErrorHendler = require("./middlewares/errors/basic.mw");
 const tokenErrorHendler = require("./middlewares/errors/tokenError.mw");
 
-
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
 app.use(tokenErrorHendler);
 app.use(basicErrorHendler);
 
-
-module.exports.app = app;
+module.exports = app;

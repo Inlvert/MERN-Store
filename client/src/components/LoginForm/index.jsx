8ce8792c58533d41b785bd4e3c6c9f5bd21e194f
Boris Ushakov <inlvert@gmail.com>
2025-04-15 13:59:36 +0300

    add basic login form comp.

---
+++ client/src/components/LoginForm/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Field, Form, Formik } from "formik";
+import { useDispatch } from "react-redux";
+import * as yup from "yup";
+
+const LOGIN_SCHEMA = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
+const initialValues = {
+  email: "test1@mail.com",
+  password: "test12345",
+};
+
+function LoginForm(props) {
+  // const dispatch = useDispatch();
+
+  const hendleSubmit = (values, formikBag) => {
+    formikBag.resetForm();
+  };
+
+  return (
+    <>
+      <div>LoginForm</div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={LOGIN_SCHEMA}
+        onSubmit={hendleSubmit}
+      >
+        <Form>
+          <Field name="email" type="email" placeholder="Email" />
+          <Field name="password" type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </Form>
+      </Formik>
+    </>
+  );
+}
+
+export default LoginForm;

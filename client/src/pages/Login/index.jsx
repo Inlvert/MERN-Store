b25813f392742aa63ffb7de41daef9a44dbc8607
Boris Ushakov <inlvert@gmail.com>
2025-04-15 13:57:00 +0300

    add basic components - page login + registration + home

---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header'
+import LoginForm from '../../components/LoginForm'
+
+function LoginPage() {
+  return (
+    <div>
+      <Header/>
+      <h1>LoginPage</h1>
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ client/src/pages/Registration/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+function RegistartionPage() {
+  return (
+    <div>
+      <Header/>
+      <h1>RegistartionPage</h1>
+    </div>
+  )
+}
+
+export default RegistartionPage

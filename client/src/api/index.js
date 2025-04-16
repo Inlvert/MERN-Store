6a006d40cdaef5bcfc718db45131bec02cca803d
Boris Ushakov <inlvert@gmail.com>
2025-04-16 10:15:08 +0300

    add interceptors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,8 +5,65 @@ const httpClient = axios.create({
   baseURL: CONSTANTS.HTTP_SERVER_URL,
 });
 
-export const registration = (userData) => httpClient.post("/auth/registration", userData);
+let accessToken = null;
+
+// Add a request interceptor
+httpClient.interceptors.request.use(
+  function (config) {
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+httpClient.interceptors.response.use(
+  function (response) {
+    if (response?.data?.data?.tokenPair) {
+      const { tokenPair } = response.data.data;
+
+      accessToken = tokenPair.accessToken;
+
+      localStorage.setItem("refreshToken", tokenPair.refreshToken);
+    }
+    return response;
+  },
+  async function (error) {
+    const {
+      response: { status },
+    } = error;
+    const refreshTokenFromLS = localStorage.getItem("refreshToken");
+
+    if (refreshTokenFromLS && status === 419) {
+      const {
+        data: {
+          data: { tokenPair },
+        },
+      } = await axios.post(`${CONSTANTS.HTTP_SERVER_URL}/auth/refresh`, {
+        refreshToken: refreshTokenFromLS,
+      });
+
+      accessToken = tokenPair.accessToken;
+
+      localStorage.setItem("refreshToken", tokenPair.refreshToken);
+
+      error.config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+      return httpClient.request(error.config);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export const registration = (userData) =>
+  httpClient.post("/auth/registration", userData);
 export const login = (userData) => httpClient.post("/auth/login", userData);
-export const refresh = (token) => httpClient.post("/auth/refresh", {refreshToken: token});
-
-
+export const refresh = (token) =>
+  httpClient.post("/auth/refresh", { refreshToken: token });

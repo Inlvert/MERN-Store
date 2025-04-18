f89ee324b752f0d47547f8465c31ffe386b46aa9
Boris Ushakov <inlvert@gmail.com>
2025-04-18 18:17:58 +0300

    add bugrer button + styles

---
+++ client/src/components/Burger/index.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import styles from './Burger.module.scss'
+import classNames from 'classnames'
+
+function Burger() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const hendleClick = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const cluoseBurger = classNames(styles.burgerLine, {
+    [styles.clicked]: isMenuOpen,
+    [styles.unclicked]: !isMenuOpen
+  });
+
+  console.log(isMenuOpen)
+  
+  return (
+    <button className={styles.burger} onClick={hendleClick}>
+      <div className={cluoseBurger}></div>
+      <div className={cluoseBurger}></div>
+      <div className={cluoseBurger}></div>
+    </button>
+  );
+}
+
+export default Burger;

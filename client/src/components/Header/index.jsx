6952abb145268006885bbdb82e5fbf7983ef28c1
Boris Ushakov <inlvert@gmail.com>
2025-04-22 00:09:01 +0300

    add  styles  for ThemeToggler in Header comp.

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -6,17 +6,43 @@ import { clearToken } from "../../api";
 import style from "./Header.module.scss";
 import Burger from "../Burger";
 import classNames from "classnames";
+import ThemeToggler from "../ThemeToggler";
 
 function Header(props) {
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { theme } = useSelector((state) => state.theme);
+
+  console.log(theme);
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const themeClassBackground = classNames(style.headerCover, {
+    [style.light]: theme === "light",
+    [style.dark]: theme === "dark",
+  });
+
+  const themeClassLinkColor = classNames(style.navLink, {
+    [style.navLinkLight]: theme === "light",
+    [style.navLinkDark]: theme === "dark",
+  });
+
+  const themeClassGreetingColor = classNames({
+    [style.greetingLight]: theme === "light",
+    [style.greetingDark]: theme === "dark",
+  });
 
   const menuClass = classNames(style.navItem, {
     [style.hiddenDisplay]: isMenuOpen,
   });
+
+  const themeClasshiddenDisplayColor = classNames({
+    [style.hiddenDisplayLight]: theme === "light",
+    [style.hiddenDisplayDark]: theme === "dark",
+  });
+
+  const mixClass = classNames(menuClass, themeClasshiddenDisplayColor)
 
   const hendleLogut = () => {
     clearToken();
@@ -24,29 +50,32 @@ function Header(props) {
   };
 
   return (
-    <header className={style.headerCover}>
+    <header className={themeClassBackground}>
       <nav className={style.nav}>
-        <a href="/" className={style.navLink}>
+        <a href="/" className={themeClassLinkColor}>
           <h3>Store</h3>
         </a>
-        <ul className={menuClass}>
+        <ul className={mixClass}>
           <li>
-            <NavLink to="/" end className={style.navLink}>
+            <ThemeToggler />
+          </li>
+          <li>
+            <NavLink to="/" end className={themeClassLinkColor}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/login" end className={style.navLink}>
+            <NavLink to="/login" end className={themeClassLinkColor}>
               Login
             </NavLink>
           </li>
           <li>
-            <NavLink to="/registration" end className={style.navLink}>
+            <NavLink to="/registration" end className={themeClassLinkColor}>
               Registration
             </NavLink>
           </li>
           <li>
-            <span style={{ color: "white" }}>
+            <span className={themeClassGreetingColor}>
               Hello {user ? `${user.firstName} ${user.lastName}` : "Guest"}
             </span>
           </li>

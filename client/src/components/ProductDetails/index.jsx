7caeec331f7988f2134f26a9611aabb34e72e549
Boris Ushakov <inlvert@gmail.com>
2025-05-05 23:38:55 +0300

    add change to ProductDetails comp

--- client/src/components/ProductDetails/index.jsx
+++ client/src/components/ProductDetails/index.jsx
@@ -31,31 +31,47 @@ function ProductDetails() {
       <h1>ProductDetails</h1>
       {product ? (
         <>
-          <h2>{product.name}</h2>
-          <div className={styles.mainImage}>
-            <img src={mainImage} alt="Main product" className={styles.largeImage} />
-          </div>
-
-          <div className={styles.thumbnails}>
-            {product.images?.length > 0 ? (
-              product.images.map((image, index) => (
+          <div className={styles.cover}>
+            <div className={styles.imageSideLeft}>
+              <div className={styles.mainImage}>
                 <img
-                  key={index}
-                  src={`${CONSTANTS.HTTP_SERVER_URL}/images/${image}`}
-                  alt={`Thumbnail ${index + 1}`}
-                  className={styles.thumbnail}
-                  onClick={() =>
-                    setMainImage(`${CONSTANTS.HTTP_SERVER_URL}/images/${image}`)
-                  }
+                  src={mainImage}
+                  alt="Main product"
+                  className={styles.largeImage}
                 />
-              ))
-            ) : (
-              <img
-                src={placeholderImg}
-                alt="No image"
-                className={styles.thumbnail}
-              />
-            )}
+              </div>
+
+              <div className={styles.thumbnails}>
+                {product.images?.length > 0 ? (
+                  product.images.map((image, index) => (
+                    <img
+                      key={index}
+                      src={`${CONSTANTS.HTTP_SERVER_URL}/images/${image}`}
+                      alt={`Thumbnail ${index + 1}`}
+                      className={styles.thumbnail}
+                      onClick={() =>
+                        setMainImage(
+                          `${CONSTANTS.HTTP_SERVER_URL}/images/${image}`
+                        )
+                      }
+                    />
+                  ))
+                ) : (
+                  <img
+                    src={placeholderImg}
+                    alt="No image"
+                    className={styles.thumbnail}
+                  />
+                )}
+              </div>
+            </div>
+            <div className={styles.imageSideRight}>
+              <h1>{product.name}</h1>
+              <h2>{product.category}</h2>
+              <p>{product.description}</p>
+              <h3>{product.price} - $</h3>
+              <button>add to cart</button>
+            </div>
           </div>
         </>
       ) : (

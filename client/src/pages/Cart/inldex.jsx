a217a014e82462645bbf9865897a59a8727c5cc9
Boris Ushakov <inlvert@gmail.com>
2025-05-06 18:54:04 +0300

    add Cart page comp

---
+++ client/src/pages/Cart/inldex.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const CartPage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>CartPage</h1>
+    </div>
+  )
+}
+
+export default CartPage

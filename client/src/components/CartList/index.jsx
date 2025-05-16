760a57eda391f6007a67c126fb031c6f9a803503
Boris Ushakov <inlvert@gmail.com>
2025-05-16 23:09:16 +0300

    add change to cartList comp

--- client/src/components/CartList/index.jsx
+++ client/src/components/CartList/index.jsx
@@ -5,7 +5,7 @@ import CONSTANTS from "../../constants";
 import placeholderImg from "../../assets/placeholder.png";
 import styles from "./CartList.module.scss";
 import { updateQuantity } from "../../redux/slices/cartProductSlice";
-
+import { NavLink } from "react-router";
 
 const CartList = () => {
   const products = useSelector((state) => state.cart.cartProducts);
@@ -13,7 +13,7 @@ const CartList = () => {
   const totalPrice = useSelector((state) => state.cart.totalPrice);
   const dispatch = useDispatch();
 
-  console.log('products is:', products.length)
+  console.log("products is:", products.length);
 
   const [quantity, setQuantity] = useState({});
 
@@ -24,40 +24,38 @@ const CartList = () => {
   }, [dispatch, cartId]);
 
   useEffect(() => {
-    if(products.length > 0) {
+    if (products.length > 0) {
       const initialQuantities = {};
       products.forEach((item) => {
         initialQuantities[item._id] = item.quantity;
       });
       setQuantity(initialQuantities);
-      console.log('initialQuantities', initialQuantities)
+      console.log("initialQuantities", initialQuantities);
     }
-  }, [products])
-  
+  }, [products]);
+
   const incrementQuantity = (cartProductId) => {
     const newQuantity = quantity[cartProductId] + 1;
     setQuantity((prev) => ({
       ...prev,
       [cartProductId]: newQuantity,
     }));
-    dispatch(updateQuantity({cartProductId, quantity: newQuantity}));
+    dispatch(updateQuantity({ cartProductId, quantity: newQuantity }));
     dispatch(getCart({ cartId }));
-  }
-
+  };
 
   const decrementQuantity = (cartProductId) => {
     const newQuantity = quantity[cartProductId] - 1;
 
-    if(newQuantity >= 1) {
+    if (newQuantity >= 1) {
       setQuantity((prev) => ({
         ...prev,
         [cartProductId]: newQuantity,
       }));
-      dispatch(updateQuantity({cartProductId, quantity: newQuantity}));
+      dispatch(updateQuantity({ cartProductId, quantity: newQuantity }));
       dispatch(getCart({ cartId }));
     }
-  }
-
+  };
 
   return (
     <div>
@@ -65,15 +63,17 @@ const CartList = () => {
       <ul className={styles.ulCover}>
         {products.map((item) => (
           <li key={item._id}>
-            <img
-              src={
-                item.product.images && item.product.images.length > 0
-                  ? `${CONSTANTS.HTTP_SERVER_URL}/images/${item.product.images[0]}`
-                  : placeholderImg
-              }
-              alt=""
-              style={{ width: "50px" }}
-            />{" "}
+            <NavLink to={`/products/${item.product._id}`}>
+              <img
+                src={
+                  item.product.images && item.product.images.length > 0
+                    ? `${CONSTANTS.HTTP_SERVER_URL}/images/${item.product.images[0]}`
+                    : placeholderImg
+                }
+                alt=""
+                style={{ width: "50px" }}
+              />
+            </NavLink>{" "}
             {item.product.name} — category: {item.product.category} — quantity:{" "}
             <button onClick={() => decrementQuantity(item._id)}>-</button>
             <input
@@ -81,7 +81,8 @@ const CartList = () => {
               value={quantity[item._id] || 1}
               className={styles.textInput}
             />
-            <button onClick={() => incrementQuantity(item._id)}>+</button>— ${item.product.price}
+            <button onClick={() => incrementQuantity(item._id)}>+</button>— $
+            {item.product.price}
           </li>
         ))}
       </ul>
